0678854f914d0fdb88b731622f395a285061a963
Linus Svensson <linusp.svensson@gmail.com>
2012-11-20 21:58:48 +0100

    Made a bass line for boogie bass. saved it in boogiebassTwinkle.midi

--- music/Haskore2010/twinkleHC.hs
+++ music/Haskore2010/twinkleHC.hs
@@ -16,8 +16,8 @@ bbf = lmap (fd qn) [f 3, c 4, d 4, c 4]
 bbg = lmap (fd qn) [g 3, d 4, e 4, d 4]
 
 -- Bassline boogie bass twinkle, one var for each line
-bl1 = foldr (:+:) [bbc, bbc, bbf, bbc, bbg, bbc, bbg, bbc]
-bl2 = foldr (:+:) [bbc, bbg, bbc, bbg, bbc, bbg, bbc, bbg]
+bl1 = foldr1 (:+:) [bbc, bbc, bbf, bbc, bbg, bbc, bbg, bbc]
+bl2 = foldr1 (:+:) [bbc, bbg, bbc, bbg, bbc, bbg, bbc, bbg]
 bl3 = bl1
 
 -- Line 1 in twinkle
@@ -36,5 +36,6 @@ l2 = p21 :+: p22 :+: p21 :+: p22
 -- Line 3 in twinkle
 l3 = l1
 
-twinkle = ( Instr "piano" (Tempo 3 (l1:+:l2:+:l3)) ) :=: (Instr "Acoustic Bass" (Tempo 3 (bl1:+:bl2:+:bl3)) )
+twinkle = Instr "Acoustic Bass" (Tempo 3 (bl1:+:bl2:+:bl3))
+--twinkle = ( Instr "piano" (Tempo 3 (l1:+:l2:+:l3)) ) :=: (Instr "Acoustic Bass" (Tempo 3 (bl1:+:bl2:+:bl3)) )
 
