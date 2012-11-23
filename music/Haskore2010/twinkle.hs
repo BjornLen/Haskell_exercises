4f96f43490938ed12af1af75b4bb04e4ec8f9988
Linus Svensson <linusp.svensson@gmail.com>
2012-11-23 16:32:48 +0100

    Added a testfunction to generate bass lines with different keys

--- music/Haskore2010/twinkle.hs
+++ music/Haskore2010/twinkle.hs
@@ -1,6 +1,6 @@
 module Twinkle where
-import AutoComp
-import Haskore
+import AutoComp 
+import Haskore hiding (Major,Minor)
  
  -- note updaters for mappings
 fd d n = n d v
@@ -15,7 +15,7 @@ times n m = m :+: (times (n - 1) m)
 -- Defining the twinkle melody --
 -- Line 1 in twinkle
 twinkleMelody :: Music
-oct = 7 
+oct = 5
 p11 = lmap (fd qn) [c oct, c oct, g oct, g oct, a oct, a oct ]
 p12 = lmap (fd hn) [g oct]
 p13 = lmap (fd qn) [f oct, f oct, e oct, e oct, d oct, d oct]
@@ -30,7 +30,7 @@ l2 = p21 :+: p22 :+: p21 :+: p22
 -- Line 3 in twinkle
 l3 = l1
 
-twinkleMelody = l1 :+: l2 :+: l3
+twinkleMelody = Instr "piano" (l1 :+: l2 :+: l3)
 
 -- Defining the chordProgression of twinkle
 twinkleChords :: ChordProgression
@@ -38,6 +38,7 @@ twinkleChords = [(C,wn),(F,hn),(C,hn),(G,hn),(C,hn),(G,hn),(C,hn),
 	(C,hn),(G,hn),(C,hn),(G,hn),(C,hn),(G,hn),(C,hn),(G,hn),
 	(C,wn),(F,hn),(C,hn),(G,hn),(C,hn),(G,hn),(C,hn)]
 
-twinkleBass = autoBass basic (C, major) twinkleChords
+twinkleBass :: (PitchClass,HarmonicQuality) -> Music
+twinkleBass key = Tempo 3 (autoBass boogie key twinkleChords)
 
-twinkle = Tempo 3 ((Instr "piano" twinkleMelody) :=: (Instr "Acoustic Bass" twinkleBass))
+twinkle = Tempo 3 (twinkleMelody :=: autoComp boogie (F,Major) twinkleChords)
