47a8c6d3b85a6e8839101a6f0fbe0264cc7a0b5c
BjornNAL <b.lennernas.programming@gmail.com>
2012-12-09 12:51:22 +0100

    Statement can now be parsed. TODO: Add statement execution

--- parsing/Statement.hs
+++ parsing/Statement.hs
@@ -6,7 +6,8 @@ import qualified Expr
 type T = Statement
 data Statement =
     Assignment String Expr.T |
-    Skip | --    Begin [Statement] |
+    Skip | 
+    Begin [Statement] |
     If Expr.T Statement Statement |
     While Expr.T Statement |
     Read String |
@@ -16,8 +17,8 @@ data Statement =
 assignment = word #- accept ":=" # Expr.parse #- require ";" >-> buildAss
 buildAss (v, e) = Assignment v e
 
-skip = accept ";" buildSkip
-buildSkip = Skip
+skip = accept "skip;" >-> buildSkip 
+buildSkip(v) = Skip
 
 if_statement = accept "if" -# Expr.parse  #- require "then" # stmt #- require "else" # stmt >-> buildIf
 buildIf((a, s1), s2) = If a s1 s2 
@@ -28,10 +29,14 @@ build_while(e, s) = While e s
 read = accept "read" -# word #- require ";" >-> build_read
 build_read(v) = Read v
 
-write = accept "write" -# word #- require ";" >-> build_write
+write = accept "write" -# Expr.parse  #- require ";" >-> build_write
 build_write(v) = Write v
 
-stmt = error "not implemented" 
+begin_stmt = accept "begin" -# iter stmt  #- require "end" >-> build_begin
+build_begin(s) = Begin s
+
+
+stmt = assignment ! Statement.read ! write ! if_statement ! while_statement ! skip ! begin_stmt
 
 
 exec :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer]
@@ -41,5 +46,5 @@ exec (If cond thenStmts elseStmts: stmts) dict input =
     else exec (elseStmts: stmts) dict input
 
 instance Parse Statement where
-  parse = stmt -- error "Statement.parse not implemented"
+  parse = stmt
   toString = error "Statement.toString not implemented"
